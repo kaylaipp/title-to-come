c1630c95a3ffbf59004944d101c1a0bd812faa60
kaylaipp <kaylaipp@bu.edu>
2021-01-17 20:54:27 -1000

    Revert "added auth"
    This reverts commit 0bc81a4b4872b9de34d74f31e2c9adff83406d6a.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
+        <h1>Hello from V2</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <AmplifySignOut/>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
